80b46c4d082eecfa8928d279d253239d2fd75671
uas-92 <uas-92@mail.ru>
2023-01-29 02:25:26 +0700

    Добавление метода инициализации массива в консоли

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//Написать программу, которая из имеющегося массива строк
+//формирует массив из строк, длинной <= 3
+
+int GetNumber(string message)
+{
+    int result;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не число.");
+        }
+    }
+
+    return result;
+}
